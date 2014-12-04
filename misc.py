40c5ddda29e9e415df5cfeeaab074452314ceb8c
Massimo Gervasini <mgervasini@mozilla.com>
2014-12-03 16:34:12 -0800

    updated scriptName

--- misc.py
+++ misc.py
@@ -722,7 +722,7 @@ def makeMHFactory(config, pf, mh_cfg=None, extra_args=None, **kwargs):
     factory = factory_class(
         scriptRepo=scriptRepo,
         interpreter=mh_cfg.get('mozharness_python', pf.get('mozharness_python')),
-        scriptName='scripts/desktop_l10n.py',
+        scriptName=mh_cfg['script_name'],
         reboot_command=mh_cfg.get('reboot_command', pf.get('reboot_command')),
         extra_args=extra_args,
         script_timeout=mh_cfg.get('script_timeout', pf.get('timeout', 3600)),
